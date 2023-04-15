cb5a7d8dd2f3932ef6293460afb1f1686f1f1561
Onyjo345 <onyjo345@student.liu.se>
2023-04-15 20:56:27 +0200

    Encrypting user password using bcrypt

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -17,5 +18,14 @@ const userSchema = mongoose.Schema({
         required: true,
     }
 });
+
+userSchema.pre('save', async function(next){
+    if (this.isModified('password')){
+       this.password = await bcrypt.hash(this.password, 10);
+          
+    }
+
+    next();
+})
 
 module.exports = mongoose.model("User", userSchema);
