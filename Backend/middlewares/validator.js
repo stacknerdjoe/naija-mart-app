d8edd6877950d924df2a4aeeecbc324eb37a021e
Onyjo345 <onyjo345@student.liu.se>
2023-04-16 09:46:19 +0200

    Validating user details using Express Validator

---
+++ Backend/middlewares/validator.js
@@ -0,0 +1,22 @@
+const {check, validationResult } = require ('express-validator');
+
+exports.userValidator = [
+    check('name').trim().not().isEmpty().withMessage('No name found!'),
+    check('email').normalizeEmail().isEmail().withMessage('Invalid email address!'),
+    check('password')
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage('No password found!')
+    .isLength({min: 6, max: 18})
+    .withMessage('Password must have at least 6 characters!')
+];
+
+exports.validate = (req, res, next) => {
+    const error = validationResult(req).array();
+    if(error.length){
+        return res.json({error: error[0].msg})
+    }
+
+    next();
+};
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,7 +1,12 @@
 const express = require ('express');
+
+
 const { create } = require("../controller/user");
+const { userValidator, validate } = require('../middlewares/validator');
+// const { validate } = require('../models/user');
 
 const router = express.Router();
-router.post ('/create', create);
+
+router.post ('/create', userValidator, validate, create);
 
 module.exports = router;
