62d539f513cb0ca81a66e772353efc1f8365d35c
Onyjo345 <onyjo345@student.liu.se>
2023-04-29 19:35:32 +0200

    Creating forget password form

--- frontend/src/components/auth/ForgetPassword.jsx
+++ frontend/src/components/auth/ForgetPassword.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import Container from '../container'
+import Title from '../form/Title'
+import FormInputer from '../form/FormInputer'
+import Submit from '../form/Submit'
+import MyLinks from '../MyLinks'
 
 export default function ForgetPassword() {
-  return (
-    <div>ForgetPassword</div>
-  )
+    return (
+        <div className='fixed inset-0 bg-primary -z-10 flex justify-center items-center'>
+            <Container>
+                <form className='bg-secondary rounded p-6 w-96 space-y-6'>
+                    <Title>Please Enter Your Email</Title>
+                    <FormInputer label='Email' placeholder='johndoe@email.com' name='email' />
+                    
+                    <Submit value='Send Link' />
+
+                    <div className="flex justify-between">
+                        <MyLinks to='/auth/signin'>Sign in</MyLinks>
+                        <MyLinks to='/auth/signup'>Sign up</MyLinks>
+                    </div>
+                </form>
+            </Container>
+        </div>
+    )
 }
