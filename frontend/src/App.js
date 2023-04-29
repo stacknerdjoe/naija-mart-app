9f7a41c2395c6690b3e868828e9c66122bc46d31
Onyjo345 <onyjo345@student.liu.se>
2023-04-29 18:12:08 +0200

    Creating Sign in form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import Signin from './components/auth/Signin'
 import Navbar from './components/user/Navbar'
 
+
 export default function App() {
-  return  < Navbar/>;
-  
+  return (
+    <>
+      < Navbar />
+      <Signin />
+    </>
+  );
 }
--- frontend/src/components/user/Navbar.jsx
+++ frontend/src/components/user/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import{BsSun} from 'react-icons/bs';
+import Container from '../container';
 
 export default function Navbar() {
-  return <div className="bg-secondary">
-    <div className="max-w-screen-xl mx-auto ">
-        <div className="flex justify-between items-center">
-
-       
-        <img src="./logo2.png" alt="" className='h-12' />
+  return (
+  <div className="bg-secondary shadow-sm shadow-gray-500">
+    <Container className="p-1">
+        <div className='flex justify-between items-center'>
+         <img src="./logo2.png" alt="" className='h-12' />
         
          <ul className='flex items-center space-x-5'>
             <li>
@@ -23,7 +23,7 @@ export default function Navbar() {
            <li className='text-white font font-semibold text-lg'>Login</li>
          </ul>
         </div>
-     </div>
+     </Container>
   </div>
-  
+  );
 }
