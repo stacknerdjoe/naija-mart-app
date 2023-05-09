5d24dc977dc695496df67dccbf0feb2237fc2637
Onyjo345 <onyjo345@student.liu.se>
2023-05-09 21:00:04 +0200

    initializing theme

--- frontend/src/components/auth/ConfirmPassword.jsx
+++ frontend/src/components/auth/ConfirmPassword.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import Container from '../container'
+import Title from '../form/Title'
+import FormInputer from '../form/FormInputer'
+import Submit from '../form/Submit'
 
 export default function ConfirmPassword() {
   return (
-    <div>ConfirmPassword</div>
+    <div className='fixed inset-0 bg-primary -z-10 flex justify-center items-center'>
+            <Container>
+                <form className='bg-secondary rounded p-6 w-96 space-y-6'>
+                    <Title>Please Enter New Password</Title>
+                    <FormInputer label='New Password' placeholder='********' name='password' type='password'/>
+                    <FormInputer label='Confrim Password' placeholder='********' name='confrimPassword' type='password' />
+                    <Submit value='Confirm Password' />
+                </form>
+            </Container>
+        </div>
   )
 }
--- frontend/src/components/auth/Signin.jsx
+++ frontend/src/components/auth/Signin.jsx
@@ -4,10 +4,13 @@ import Title from '../form/Title';
 import FormInputer from '../form/FormInputer';
 import Submit from '../form/Submit';
 import MyLinks from '../MyLinks';
-import { Link } from 'react-router-dom';
+
 
 export default function Signin() {
-    return  <div className='fixed inset-0 bg-primary -z-10 flex justify-center items-center'>
+
+   
+
+    return <div className='fixed inset-0 bg-primary -z-10 flex justify-center items-center'>
         <Container>
             <form className='bg-secondary rounded p-6 w-72 space-y-6'>
                 <Title>Sign in</Title>
