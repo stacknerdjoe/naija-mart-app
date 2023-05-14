29e21ae1aefdc6c8ba98aa447d5cf73c547871ab
Onyjo345 <onyjo345@student.liu.se>
2023-05-14 11:13:21 +0200

    Configuring Cloudinary

--- Backend/controller/actor.js
+++ Backend/controller/actor.js
@@ -1,3 +1,16 @@
+const Actor = require('../models/actor');
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({ 
+    cloud_name: 'dmszouolz', 
+    api_key: '187299652359591', 
+    api_secret: 'jhDNrveQGpZqsPsZAb_XXGgqQ1k',
+    //secure: true
+  });
+
 exports.createActor = (req, res) => {
-    console.log(req.body)
+    const {name, about, gender} = req.body;
+    const {file} = req
+
+    const newActor = new Actor({name, about, gender})
 }
