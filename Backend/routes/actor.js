ffe24e2163c373d8ec2e072183aaf15e2c3160f4
Onyjo345 <onyjo345@student.liu.se>
2023-05-14 10:41:57 +0200

    configuring multer to handle files in backend

--- Backend/routes/actor.js
+++ Backend/routes/actor.js
@@ -2,6 +2,6 @@ const express = require ('express');
 const { createActor } = require('../controller/actor');
 const router = express.Router();
 
-router.post('/create', createActor)
+router.post('/create', uploadImage.single("avatar"), createActor)
 
 module.exports = router; 
