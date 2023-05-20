0abbe84809bbc76e332a624f9242726678dea19b
Onyjo345 <onyjo345@student.liu.se>
2023-05-20 11:23:40 +0200

    Trailer upload route

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 require('./db');
 const userRouter = require('./routes/user');
 const actorRouter = require('./routes/actor');
+const movieRouter = require('./routes/movie');
 
 const { errorHandler } = require('./middlewares/error');
 const cors = require('cors');
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use('/api/user', userRouter);
 app.use('/api/actor', actorRouter);
+app.use('/api/movie', movieRouter);
 
 app.use('/*', handleNotFound);
 
