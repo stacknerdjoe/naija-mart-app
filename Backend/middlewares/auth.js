66e43648b4d0dda8f6d5f4dc6ee6986bd824af06
Onyjo345 <onyjo345@student.liu.se>
2023-05-20 10:55:40 +0200

    movie model

--- Backend/middlewares/auth.js
+++ Backend/middlewares/auth.js
@@ -1,20 +1,20 @@
-const jwt = require("jsonwebtoken");
-const { sendError } = require("../utils/helper");
-const User = require("../models/user");
+const jwt = require('jsonwebtoken');
+const { sendError } = require('../utilities/helper');
+const User = require('../models/user');
 
 exports.isAuth = async (req, res, next) => {
   const token = req.headers?.authorization;
 
-  if(!token)return sendError(res, "Invalid token!");
+  if(!token)return sendError(res, 'Invalid token!');
 
-  const jwtToken = token.split("Bearer ")[1];
+  const jwtToken = token.split('Bearer ')[1];
 
-  if (!jwtToken) return sendError(res, "Invalid token!");
+  if (!jwtToken) return sendError(res, 'Invalid token!');
   const decode = jwt.verify(jwtToken, );
   const { userId } = decode;
 
   const user = await User.findById(userId);
-  if (!user) return sendError(res, "unauthorized access!");
+  if (!user) return sendError(res, 'unauthorized access!');
 
   req.user = user;
 
@@ -22,8 +22,9 @@ exports.isAuth = async (req, res, next) => {
 };
 
 exports.isAdmin = (req, res, next) => {
-    
 
+  const { user } = req;
+  if (user.role !== 'admin') return sendError(res, 'unauthorized access!');
 
     next();
 
