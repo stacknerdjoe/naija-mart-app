cf952991a819cd8e666a3a7a0943fa503f69f4f1
Onyjo345 <onyjo345@student.liu.se>
2023-05-20 11:23:24 +0200

    Trailer upload route

---
+++ Backend/routes/movie.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { isAdmin, isAuth } = require('../middlewares/auth');
+const { uploadVideo } = require('../middlewares/multer');
+const { uploadTrailer } = require('../controller/movie');
+
+const router = express.Router();
+
+router.post(
+    "/upload-trailer", isAuth, isAdmin, uploadVideo.single("video"), uploadTrailer
+);
+
+
+module.exports = router; 
