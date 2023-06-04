0aa4018bcf1b6103ce56c35f8b797e49505d6fff
Onyjo345 <onyjo345@student.liu.se>
2023-06-04 17:03:28 +0200

    Screencast link

--- frontend/src/components/admin/MovieUpload.jsx
+++ frontend/src/components/admin/MovieUpload.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import { FileUploader } from 'react-drag-drop-files';
-import { AiOutlineCloudUpload } from 'react-icons/ai';
-import { uploadMovie, uploadTrailer } from '../../api/movie';
-import { useNotification } from '../../hooks';
-import ModalContainer from '../models/ModalContainer';
-import MovieForm from './MovieForm';
+import React, { useState } from "react";
+import { FileUploader } from "react-drag-drop-files";
+import { AiOutlineCloudUpload } from "react-icons/ai";
+import { uploadMovie, uploadTrailer } from "../../api/movie";
+import { useNotification } from "../../hooks";
+import ModalContainer from "../models/ModalContainer";
+import MovieForm from "./MovieForm";
 
 export default function MovieUpload({ visible, onClose }) {
   const [videoSelected, setVideoSelected] = useState(false);
@@ -16,7 +16,7 @@ export default function MovieUpload({ visible, onClose }) {
   const { updateNotification } = useNotification();
 
   const handleTypeError = (error) => {
-    updateNotification('error', error);
+    updateNotification("error", error);
   };
 
   const handleUploadTrailer = async (data) => {
@@ -24,7 +24,7 @@ export default function MovieUpload({ visible, onClose }) {
       data,
       setUploadProgress
     );
-    if (error) return updateNotification('error', error);
+    if (error) return updateNotification("error", error);
 
     setVideoUploaded(true);
     setVideoInfo({ url, public_id });
@@ -32,7 +32,7 @@ export default function MovieUpload({ visible, onClose }) {
 
   const handleChange = (file) => {
     const formData = new FormData();
-    formData.append('video', file);
+    formData.append("video", file);
 
     setVideoSelected(true);
     handleUploadTrailer(formData);
@@ -40,7 +40,7 @@ export default function MovieUpload({ visible, onClose }) {
 
   const getUploadProgressValue = () => {
     if (!videoUploaded && uploadProgress >= 100) {
-      return 'Processing';
+      return "Processing";
     }
 
     return `Upload progress ${uploadProgress}%`;
@@ -85,13 +85,13 @@ const TrailerSelector = ({ visible, handleChange, onTypeError }) => {
   if (!visible) return null;
 
   return (
-    <div className='h-full flex items-center justify-center'>
+    <div className="h-full flex items-center justify-center">
       <FileUploader
         handleChange={handleChange}
         onTypeError={onTypeError}
-        types={['mp4', 'avi']}
+        types={["mp4", "avi"]}
       >
-        <div className='w-48 h-48 border border-dashed dark:border-dark-subtle border-light-subtle rounded-full flex flex-col items-center justify-center dark:text-dark-subtle text-secondary cursor-pointer'>
+        <div className="w-48 h-48 border border-dashed dark:border-dark-subtle border-light-subtle rounded-full flex flex-col items-center justify-center dark:text-dark-subtle text-secondary cursor-pointer">
           <AiOutlineCloudUpload size={80} />
           <p>Drop your file here!</p>
         </div>
@@ -104,14 +104,14 @@ const UploadProgress = ({ width, message, visible }) => {
   if (!visible) return null;
 
   return (
-    <div className='dark:bg-secondary bg-white drop-shadow-lg rounded p-3'>
-      <div className='relative h-3 dark:bg-dark-subtle bg-light-subtle overflow-hidden'>
+    <div className="dark:bg-secondary bg-white drop-shadow-lg rounded p-3">
+      <div className="relative h-3 dark:bg-dark-subtle bg-light-subtle overflow-hidden">
         <div
-          style={{ width: width + '%' }}
-          className='h-full absolute left-0 dark:bg-white bg-secondary'
+          style={{ width: width + "%" }}
+          className="h-full absolute left-0 dark:bg-white bg-secondary"
         />
       </div>
-      <p className='font-semibold dark:text-dark-subtle text-light-subtle animate-pulse mt-1'>
+      <p className="font-semibold dark:text-dark-subtle text-light-subtle animate-pulse mt-1">
         {message}
       </p>
     </div>
